74f2bd1afdb3a3a60073a7145daaafc2eff0cb55
Sebastian Zack Tin Lahm-Lee <bctnry@sdf.org>
2025-06-13 15:09:10 +0100

    refactor: minor clean up

--- pkg/aegis/session/dbif.go
+++ pkg/aegis/session/dbif.go
@@ -16,7 +16,7 @@ type AegisSessionStore interface {
 const passchdict = "abcdefghijklmnopqrstuvwxyz0123456789"
 func NewSessionString() string {
 	res := make([]byte, 0)
-	for _ = range 48 {
+	for range 48 {
 		res = append(res, passchdict[rand.Intn(len(passchdict))])
 	}
 	return string(res)
