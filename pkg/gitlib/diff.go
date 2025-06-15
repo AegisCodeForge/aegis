d08ed998e99a1891554e8de54af982b6243a9016
Sebastian Zack Tin Lahm-Lee <bctnry@sdf.org>
2025-06-15 20:36:11 +0100

    fix: dubious ownership

--- pkg/gitlib/diff.go
+++ pkg/gitlib/diff.go
@@ -1,7 +1,10 @@
 package gitlib
 
 import (
+	"bytes"
+	"errors"
 	"os/exec"
+	"strings"
 )
 
 type DiffItem struct {
@@ -11,11 +14,19 @@ type DiffItem struct {
 // returns the diff by invoking the "git diff" command.
 // i should probably implement my own diff and rely as little on the
 // git executable as possible at some point.
+var ErrDubiousOwnership = errors.New("dubious ownership")
 func (gr LocalGitRepository) GetDiff(commitId string) (string, error) {
 	cmd := exec.Command("git", "diff-tree", commitId, "-p")
 	cmd.Dir = gr.GitDirectoryPath
+	stderrBuf := new(bytes.Buffer)
+	cmd.Stderr = stderrBuf
 	out, err := cmd.Output()
-	if err != nil { return "", err }
+	if err != nil {
+		if strings.Contains(stderrBuf.String(), "dubious ownership") {
+			return "", ErrDubiousOwnership
+		}
+		return "", err
+	}
 	return string(out), nil
 }
 
