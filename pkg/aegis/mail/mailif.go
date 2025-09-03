86742b4912de63e189a2e68915d19d9668c31d4b
Sebastian Zack Tin Lahm-Lee <bctnry@sdf.org>
2025-09-03 21:28:17 +0100

    feat: mailer tester in admin panel

--- pkg/aegis/mail/mailif.go
+++ pkg/aegis/mail/mailif.go
@@ -16,11 +16,15 @@ type AegisMailerInterface interface {
 var ErrNotSupported = errors.New("Type not supported.")
 
 func InitializeMailer(cfg *aegis.AegisConfig) (AegisMailerInterface, error) {
-	switch cfg.Mailer.Type {
+	return CreateMailerFromMailerConfig(&cfg.Mailer)
+}
+
+func CreateMailerFromMailerConfig(cfg *aegis.AegisMailerConfig) (AegisMailerInterface, error) {
+	switch cfg.Type {
 	case "gmail-plain":
-		return gmail_plain.NewAegisGmailPlainMailerInterface(cfg.Mailer.User, cfg.Mailer.Password)
+		return gmail_plain.NewAegisGmailPlainMailerInterface(cfg.User, cfg.Password)
 	case "smtp":
-		return smtp_plain.NewAegisSMTPPlainMailerInterface(cfg.Mailer.SMTPServer, cfg.Mailer.SMTPPort, cfg.Mailer.SMTPAuth, cfg.Mailer.User, cfg.Mailer.Password)
+		return smtp_plain.NewAegisSMTPPlainMailerInterface(cfg.SMTPServer, cfg.SMTPPort, cfg.SMTPAuth, cfg.User, cfg.Password)
 	}
 	return nil, ErrNotSupported
 }
