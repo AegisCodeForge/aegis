e12e2d7c6e10eaa2fc75e5abd7f09a5aeb4fd517
Sebastian Zack Tin Lahm-Lee <bctnry@sdf.org>
2025-12-29 23:04:24 +0000

    fix: finally

--- routes/controller/aux.go
+++ routes/controller/aux.go
@@ -249,8 +249,8 @@ func newConfirmCode() string {
 func getQueryPath(s string) (string, error) {
 	u, err := url.Parse(s)
 	if err != nil { return "", err }
-	return fmt.Sprintf("%s?%s",
-		url.QueryEscape(u.Path),
+	return fmt.Sprintf("/%s?%s",
+		url.QueryEscape(u.Path[1:]),
 		url.QueryEscape(u.Query().Encode())), nil
 }
 
